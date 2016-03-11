da322244c5f480ae9789e93323b97223fcf1048d
valmat <ufabiz@gmail.com>
2016-03-11 18:10:47 +0600

    update example 'Inverse2.ino'

--- examples/Inverse2/Inverse2.ino
+++ examples/Inverse2/Inverse2.ino
@@ -5,41 +5,38 @@
 // pin 10 is connected to LOAD 
 LedMatrix matrix(12,11,10);
 
-const uint16_t delaytime = 300;
-
 void setup()
 {
     // Set the brightness. (0..15)
     matrix.setIntensity(0);
 }
 
-
 void loop()
 {
     // Switch all LEDs on the display to off.
     matrix.clear();
-    delay(delaytime);
+    delay(300);
 
     // Switch all LEDs on the display to on.
     matrix.fill();
-    delay(delaytime);
+    delay(300);
 
     // Iterate over rows to invert them
     for(auto &row: matrix.rows()) {
         matrix.invert(row);
-        delay(delaytime);
+        delay(70);
     }
     // Iterate over colomns to invert them
     for(auto &col: matrix.cols()) {
         matrix.invert(col);
-        delay(delaytime);
+        delay(70);
     }
 
     // Iterate for each point to invert it
     for(auto &row: matrix.rows()) {
         for(auto &col: matrix.cols()) {
             matrix.invert(row, col);
-            delay(delaytime);
+            delay(30);
         }
     }
 
