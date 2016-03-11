a2776e5d53f67d19025cb3f266c1bfe0e755b92e
valmat <ufabiz@gmail.com>
2016-03-11 19:22:42 +0600

    Updated example TestRotation.ino

---
+++ examples/TestRotation/TestRotation.ino
@@ -0,0 +1,26 @@
+#include "LedMatrix.h"
+
+// pin 12 is connected to the DataIn 
+// pin 11 is connected to the CLK 
+// pin 10 is connected to LOAD 
+LedMatrix matrix(12,11,10);
+
+const uint16_t delaytime = 1000;
+
+void setup() 
+{
+    // Set the brightness. (0..15)
+    matrix.setIntensity(1);
+}
+
+
+void loop()
+{
+    matrix.set({B00111111, B00011111, B00001111, B00011111, B00110011, B01100001, B11000111, B10000111});
+    delay(delaytime);
+    
+    matrix.invert();
+    delay(delaytime);
+    
+    matrix.setRotation();
+}
